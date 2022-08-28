39d2194571ab395445821b4f66de0cb2da16053d
Pradhyumna789 <45138354+Pradhyumna789@users.noreply.github.com>
2022-08-28 20:22:54 +0530

    Add files via upload

---
+++ sumOfDigits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n) {
+
+    if (n == 0) {
+        
+        return n;
+        
+    }
+    
+    return (n % 10) + sumOfDigits(n/10);
+    
+    
+}
+
+
+
+
+int main() {
+   int n;
+   cin >> n;
+   cout << sumOfDigits(n) << endl;
+}
