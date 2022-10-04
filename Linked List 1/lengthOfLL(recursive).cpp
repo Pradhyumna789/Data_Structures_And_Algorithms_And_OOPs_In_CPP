a069f40a4d1dd663980a2006d22755168f928a9c
Pradhyumna789 <45138354+Pradhyumna789@users.noreply.github.com>
2022-10-04 23:57:28 +0530

    Add files via upload

---
+++ Linked List 1/lengthOfLL(recursive).cpp
@@ -0,0 +1,73 @@
+int length(Node *head) {
+    
+    if(head == NULL) {
+        
+        return 0;
+        
+    }
+    
+
+     return 1 + length(head -> next);
+
+
+}
+
+
+/*
+
+main code
+
+#include <iostream>
+class Node {
+   public:
+    int data;
+    Node *next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput() {
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1) {
+        Node *newNode = new Node(data);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head) {
+    Node *temp = head;
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+int main() {
+  
+    
+   
+        Node *head = takeinput();
+        cout << length(head) << "\n";
+    
+}
+
+
+*/
