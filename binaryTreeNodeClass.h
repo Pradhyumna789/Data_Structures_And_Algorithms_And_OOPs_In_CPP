ea91850932e9452671eae4a4c2cae5f03a6ff37c
Pradhyumna789 <45138354+Pradhyumna789@users.noreply.github.com>
2022-10-27 13:36:01 +0530

    Add files via upload

---
+++ binaryTreeNodeClass.h
@@ -0,0 +1,46 @@
+template <typename T>
+class BinaryTreeNode {
+
+public:
+
+T data;
+BinaryTreeNode *left;
+BinaryTreeNode *right;
+
+BinaryTreeNode(T data) {
+
+this -> data = data;
+left = NULL;
+right = NULL;
+
+}
+
+~BinaryTreeNode() {
+
+delete left;
+delete right;
+
+}
+
+
+}
+
+/*
+
+main code to test the above created class
+
+#include<iostream>
+#include "BinaryTreeNodeClass"
+using namespace std;
+
+int main() {
+
+BinaryTreeNode<int>* root = new BinaryTreeNode<int>(1);
+BinaryTreeNode<int>* node1 = new BinaryTreeNode<int>(2);
+BinaryTreeNode<int>* node2 = new BinaryTreeNode<int>(3);
+root -> left = node1;
+root -> right = node2;  
+
+}
+
+*/
