60865c7991317f503c43a2d2acebdcd8986321be
Pradhyumna789 <45138354+Pradhyumna789@users.noreply.github.com>
2022-10-30 02:01:33 +0530

    Add files via upload

---
+++ Binary Tree/heightOfABinaryTree.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int height(BinaryTreeNode<int> *root) {
+
+if (root == NULL) {
+
+return 0;
+
+}
+
+return max(height(root -> left), height(root -> right)) + 1;
+
+}
+
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    cout << height(root);
+}
