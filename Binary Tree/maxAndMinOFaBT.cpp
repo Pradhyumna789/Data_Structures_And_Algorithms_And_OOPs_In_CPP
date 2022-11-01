f8a3a2a16640f9005e403a4073fae08bc4f1dc28
Pradhyumna789 <45138354+Pradhyumna789@users.noreply.github.com>
2022-11-01 11:59:54 +0530

    Add files via upload

---
+++ Binary Tree/maxAndMinOFaBT.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+pair<int, int> pairMinAndMax(BinaryTreeNode<int> *root) {
+
+if (root == NULL) {
+
+
+
+if (root == NULL) {
+
+pair<int, int> ret = max_pair(INT_MAX, INT_MIN);
+return ret;
+
+}
+
+pair<int, int> leftPair = getMinAndMax(root -> left);
+pair<int, int> rightPair = getMinAndMax(root -> right);
+
+ret.first = min(min(leftPair.first, rightPair.first), root -> data);
+ret.second = max(max(rightPair.second, rightPair.second), root -> data);
+
+return ret;
+
+}
+
+}
+
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    pair<int, int> ans = getMinAndMax(root);
+    cout << ans.first << " " << ans.second;
+}
