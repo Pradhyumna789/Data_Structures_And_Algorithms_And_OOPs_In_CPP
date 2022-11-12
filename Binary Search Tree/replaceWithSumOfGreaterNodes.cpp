d3f436e8dce0856122c2e59a12e0fc777bf006f4
Pradhyumna789 <45138354+Pradhyumna789@users.noreply.github.com>
2022-11-13 02:19:44 +0530

    Add files via upload

---
+++ Binary Search Tree/replaceWithSumOfGreaterNodes.cpp
@@ -0,0 +1,28 @@
+int replaceWithLargerNodesSum(BinaryTreeNode<int> *root, int sum) {
+
+if (root == NULL) {
+
+return sum;
+
+}
+
+sum = replaceWithLargerNodesSum(root -> right, sum);
+
+sum = sum + root -> data;
+root -> data = sum;
+
+sum = replaceWithLargerNodesSum(root -> left, sum);
+
+return sum;
+
+}
+
+
+
+void replaceWithLargerNodesSum(BinaryTreeNode<int> *root) {
+
+int sum = 0;
+replaceWithLargerNodesSum(root, sum);
+
+
+}
